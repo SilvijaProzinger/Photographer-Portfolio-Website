385a2f80fdb4a6d89870dd99383c00e6b96adbd8
Silvija Prozinger <37271365+SilvijaProzinger@users.noreply.github.com>
2020-05-02 13:42:13 +0200

    finished landing page

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
+let landing = document.getElementById('landing')
+let work = document.getElementById('work')
+
+let workButtons = document.getElementsByClassName('work-link')
+
 //on hovering the arrow replace the image with arrow of stronger color
 document.getElementById('arrow-right').addEventListener('mouseover', function(){
 	console.log('replace')
 })
+
+/*on hovering the arrow replace the image with arrow of stronger color
+document.getElementById('arrow-left').addEventListener('mouseover', function(){
+	console.log('replace')
+})
+*/
+
+for (let i=0; i < workButtons.length; i++){
+	workButtons[i].addEventListener('click', function(){
+		landing.style.display = 'none'
+		work.style.display = 'block'
+		console.log(i)
+	})
+}
