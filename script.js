ead2595b9f373747d48edf0b3220d30a68f1bdb6
Silvija Prozinger <37271365+SilvijaProzinger@users.noreply.github.com>
2020-05-05 16:28:55 +0200

    replaced scrip with jquery script and finished carousel

--- script.js
+++ script.js
@@ -1,30 +1,62 @@
-let landing = document.getElementById('landing')
-let work = document.getElementById('work')
-
-let workButtons = document.getElementsByClassName('work-link')
+$('.work-link').on('click', function(){
+		$('#landing').css({display: 'none'})
+		$('#work').css({display: 'block'})
+})
 
 //on hovering the arrow replace the image with arrow of stronger color
-document.getElementById('arrow-right').addEventListener('mouseover', function(){
-	document.getElementById('arrow-right').src = './images/icons8-right-arrow-100-1.png'
-	document.getElementById('work-link-text').style.color = '#0f4c81'
+$('#arrow-right').on('mouseover', function(){
+	$('#arrow-right').attr('src','./images/icons8-right-arrow-100-1.png')
+	$('#work-link-text').css({color: '#0f4c81'}) 
 })
 
-//on unhovering the arrow returns to normal, lighter color
-document.getElementById('arrow-right').addEventListener('mouseout', function(){
-	document.getElementById('arrow-right').src = './images/icons8-right-arrow-100.png'
-	document.getElementById('work-link-text').style.color = '#6f93b3'
+//on unhovering the arrow replace the image with the original, lighter one
+$('#arrow-right').on('mouseout', function(){
+	$('#arrow-right').attr('src','./images/icons8-right-arrow-100.png')
+	$('#work-link-text').css({color: '#6f93b3'}) 
 })
 
-/*on hovering the arrow replace the image with arrow of stronger color
-document.getElementById('arrow-left').addEventListener('mouseover', function(){
-	console.log('replace')
-})
+//loop through carousel parent div and save first and last child so that the carousel can be endless
+$(".images").each(function(index) {
+    firstChild = $(this).children(".carousel-item:first");
+    lastChild = $(this).children(".carousel-item:last")
+});
+
+
+/*function swap(a, b) {
+    a = $(a); b = $(b);
+    var tmp = $('<span>').hide();
+    a.before(tmp);
+    b.before(a);
+    tmp.replaceWith(b);
+};
 */
 
-for (let i=0; i < workButtons.length; i++){
-	workButtons[i].addEventListener('click', function(){
-		landing.style.display = 'none'
-		work.style.display = 'block'
-		console.log(i)
-	})
-}
+//on clicking the left arrow in carousel, go left
+$('#left').on('click', function(){
+	let active = $('.central')
+	let previous = active.prev()
+
+	if (previous.length === 0){
+		previous = lastChild.removeClass('small').addClass('central')
+		firstChild.removeClass('central').addClass('small')
+	} else {
+		active.removeClass('central').addClass('small')
+		previous.addClass('central').removeClass('small')
+	}
+})
+
+$('#right').on('click', function(){
+	let active = $('.central')
+	let next = active.next()
+
+	if (next.length === 0){
+		next = firstChild.removeClass('small').addClass('central')
+		lastChild.removeClass('central').addClass('small')
+	} else {
+		active.removeClass('central').addClass('small')
+		next.addClass('central').removeClass('small')
+	}
+})
+
+
+
